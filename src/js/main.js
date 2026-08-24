231141e0300860ce8145a9a8b02e58d5e7c6ef1f
johnwarp <samuelzhao0121@gmail.com>
2026-08-24 17:17:13 -0400

    Implementing typing logic

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function init() {
   console.log('App initialized');
+
+  // DOM elements
+  const prompt = document.getElementById("typing-prompt");
+
+  // variables for typing logic
+  let sampleText = "the quick brown fox jumps over the lazy dog";
+  let inputText = "";
+
+  console.log(sampleText);
+
+  function render() {
+
+  }
+
+  function handleInput(e) {
+    let text = e.target.value;
+    let char = text[text.length - 1];
+
+    inputText += char
+
+    console.log(inputText);
+  }
+
+  prompt.addEventListener("input", handleInput);
+  
 }
