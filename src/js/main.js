f5608306b191cff7a52de34e1e38a2a794cb1461
johnwarp <samuelzhao0121@gmail.com>
2026-08-25 20:59:22 -0400

    Implemented user input + more
    Also added respective colours for upcoming, correct, and wrongly typed characters.

--- src/js/main.js
+++ src/js/main.js
@@ -13,25 +13,35 @@ function init() {
 
   // variables for typing logic
   let sampleText = "the quick brown fox jumps over the lazy dog";
+  let targetText = sampleText;
   let inputText = "";
-
-  console.log(sampleText);
+  let index = 0;
 
   function render() {
-    display.innerHTML = inputText;
+    for (let char of targetText) {
+    }
+    display.innerHTML = targetText;
   }
 
   function handleInput(e) {
-    let text = e.target.value;
-    let char = text[text.length - 1];
+    inputText = e.target.value;
 
-    inputText += char
-    console.log(inputText);
+    if (inputText.slice(0, index + 1) === targetText.slice(0, index + 1)) {
+      ++index;
+    }
+
+    if (index >= targetText.length) {
+      console.log("You win");
+      // TODO: terminate program
+    }
+
+    console.log(index, inputText);
 
     render();
   }
 
   clickZone.addEventListener("click", () => prompt.focus());
   prompt.addEventListener("input", handleInput);
-  
+
+  render();
 }
